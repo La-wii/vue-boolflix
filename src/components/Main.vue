0ed7a57f7f12d20cc5bcb302b77e1102d2486d30
La-wii <sentilavale@gmail.com>
2021-07-12 15:15:28 +0200

    header

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+    <main></main>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "Main",
+    data() {
+        return {
+            apiURL : 'https://api.themoviedb.org/3/movie/550?api_key=e23f5b7ea0860eb65056406dfeb3eda9',
+            films: [],
+        }
+    },
+    methods: {
+        getSong(){
+            axios
+            .get(this.apiURL)
+            .then (picker =>{
+                // console.log(picker);
+                this.films = picker.data.response
+                console.log(this.fimls);
+            })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
