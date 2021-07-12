0a5b62e8f56b0e1ed9176d27d56680e6869d82aa
La-wii <sentilavale@gmail.com>
2021-07-12 16:46:56 +0200

    add films

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,10 @@
             <div class="container-lg">
                 <div class="row">
                     <div class="col-2">
+                        <img src="@/img/Netflix-logo.png" alt="">
+                    </div>
+
+                    <div class="col-5">
                         
                     </div>
                 </div>
@@ -22,6 +26,21 @@ export default {
 <style scoped lang="scss">
     header{
         background-color: black;
-        height: 70px;
+        // height: 70px;
+            nav{
+                .container-lg{
+                    .row{
+                        .col-2{
+                            img{
+                                height: 50px;
+                                width: 100px;
+                                vertical-align: middle;
+                                // padding: 20px 0;
+                            }
+                        }
+                        
+                    }
+                }
+            }
     }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,36 @@
 <template>
     <main>
         <div class="container-lg">
+
+            <div class="search">
+                <div class="row">
+
+                    <div class="col-3 py-3">
+                        <input type="text">
+                        <button class="ms-2 px-2">Cerca</button>
+                    </div>
+                    
+                </div>
+            </div>
+
             <div class="row py-5">
                 <div class="col-12 d-flex flex-wrap py-5">
-                    <div v-for="(element, index) in films" :key="index" class="card" style="width: 18rem;">
+                    <div v-for="(element, index) in films" :key="index" class="card" style="width: 12rem;">
                         <!-- <img :src="" class="card-img-top" alt="..."> -->
 
                         <div class="card-body">
-                            <h5 class="card-title">{{element.title}}</h5>
-                            <p class="card-text">{{element.original_title}}</p>
-                            <p class="card-text">{{element.original_language}}</p>
-                            <p class="card-text">{{element.vote_average}}</p>
+                            <h5 class="card-title">
+                                {{element.title}}
+                            </h5>
+                            <p class="card-text">
+                                {{element.original_title}}
+                            </p>
+                            <p class="card-text">
+                                {{element.original_language}}
+                            </p>
+                            <p class="card-text">
+                                {{element.vote_average}}
+                            </p>
                         </div>
 
                     </div>
@@ -29,12 +49,12 @@ export default {
     name: "Main",
     data() {
         return {
-            apiURL : "https://api.themoviedb.org/3/movie/550?api_key=e23f5b7ea0860eb65056406dfeb3eda9",
+            apiURL : "https://api.themoviedb.org/3/movie/popular?api_key=e074c01e562b214f49b0d0b915aa74f1",
             films: [],
         }
     },
     created (){
-       this.getFilms() 
+       this.getFilms()
        
     },
     methods: {
@@ -43,7 +63,7 @@ export default {
             .get(this.apiURL)
             .then (picker =>{
                 console.log(picker);
-                this.films.push(picker.data)
+                this.films = picker.data.results
                 
                 // this.films = picker.data.title
             })
@@ -56,14 +76,28 @@ export default {
 <style scoped lang="scss">
     main{
         background-color: red;
+        // height: 1000px;
+            .container-lg{
+                .search{
+                    .row{
+                        .col-3{
+                            input{
 
-            .row{
+                            }
+                            button{
+                                
+                            }
+                        }
+                    }
+                }
+                .row{
 
-                .col-12{
+                    .col-12{
 
-                    .card{
-                    width: calc(100% / 5);
-                    margin: 20px;
+                        .card{
+                        width: calc(100% / 5);
+                        margin: 20px;
+                        }
                     }
                 }
             }
