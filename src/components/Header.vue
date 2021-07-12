b85aa3ab5224277c7eaa236f1c9e5237d838514c
La-wii <sentilavale@gmail.com>
2021-07-12 22:11:51 +0200

    add input + button header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,36 @@
     <header>
         <nav>
             <div class="container-lg">
-                <div class="row">
+                <div class="row d-flex py-3">
                     <div class="col-2">
                         <img src="@/img/Netflix-logo.png" alt="">
                     </div>
 
-                    <div class="col-5">
-                        
+                    <div class="col-6">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Serie Tv</a></li>
+                            <li><a href="#">Film</a></li>
+                            <li><a href="#">Nuovi e Popolari</a></li>
+                            <li><a href="#">La mia lista</a></li>
+                        </ul>
                     </div>
+
+                    <div class="col-4 d-inline-flex">
+
+                        <input type="text" 
+                            class="form-control" 
+                            placeholder="Scegli un titolo"
+                            v-model.trim="inputText"
+                        >
+
+                        <button type="submit" 
+                        class="ms-2 px-2"
+                        @click.prevent="$emit('searchForm',inputText)"
+                        >Cerca</button>
+
+                    </div> 
+
                 </div>
             </div>
 
@@ -20,6 +42,11 @@
 <script>
 export default {
     name: "Header",
+    data(){
+        return {
+            inputText:'',
+        }
+    } 
 }
 </script>
 
@@ -36,6 +63,19 @@ export default {
                                 width: 100px;
                                 vertical-align: middle;
                                 // padding: 20px 0;
+                            }
+                        }
+                        .col-6{
+                            ul{
+                                li{
+                                    display: inline-block;
+                                    list-style: none;
+                                    a{
+                                        text-decoration: none;
+                                        color: white;
+                                        padding: 0 10px;
+                                    }
+                                }
                             }
                         }
                         
