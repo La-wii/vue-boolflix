bf13089971b636301e019f281f45e768b4236410
La-wii <sentilavale@gmail.com>
2021-07-13 12:33:54 +0200

    costruita header e nav su search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,46 @@
 <template>
-    <div class="col-4 d-inline-flex">
 
-        <input type="text" 
-            class="form-control" 
-            placeholder="Scegli un titolo"
-            v-model.trim="inputText"
-        >
 
-        <button type="submit" 
-        class="ms-2 px-2"
-        @click.prevent="$emit('search',inputText)"
-        >Cerca</button>               
+    <header>
+        <nav>
+            <div class="container-lg">
+                <div class="row d-flex py-3">
+                    <div class="col-2">
+                        <img src="@/img/Netflix-logo.png" alt="">
+                    </div>
 
-    </div> 
+                    <div class="col-6">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Serie Tv</a></li>
+                            <li><a href="#">Film</a></li>
+                            <li><a href="#">Nuovi e Popolari</a></li>
+                            <li><a href="#">La mia lista</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-4 d-inline-flex">
+
+                    <input type="text" 
+                        class="form-control" 
+                        placeholder="Scegli un titolo"
+                        v-model.trim="inputText"
+                    >
+
+                    <button type="submit" 
+                    class="ms-2 px-2"
+                    @click.prevent="$emit('search',inputText)"
+                    >Cerca</button>               
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </nav>
+    </header>
+     
 </template>
 
 <script>
@@ -26,6 +54,43 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+
+    header{
+        background-color: black;
+        // height: 70px;
+        height: 7vh;
+            nav{
+                .container-lg{
+                    .row{
+                        .col-2{
+                            img{
+                                height: 50px;
+                                width: 100px;
+                                vertical-align: middle;
+                                // padding: 20px 0;
+                            }
+                        }
+                        .col-6{
+
+                            ul{
+
+                                li{
+                                    display: inline-block;
+                                    list-style: none;
+
+                                    a{
+                                        text-decoration: none;
+                                        color: white;
+                                        padding: 0 10px;
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
 
 </style>
